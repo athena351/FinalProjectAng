55847af2df3a94e3749bd90af6d0a4054c6344dd
athena <ketevan.dolidze.2@btu.edu.ge>
2026-04-10 23:19:49 +0400

    main

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPage } from "./main-page/main-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [MainPage]
 })
 export class App {
   protected readonly title = signal('FinalProjectAng');
